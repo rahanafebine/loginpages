6ed9fe8f5bd64db15ca74a36a38d1ac6775da358
rahanafebine <rahanafebine@gmail.com>
2021-04-23 11:27:41 +0530

    first commit

---
+++ resources/views/signin.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+<body> 
+    <img src="{{url('hd.jpg.jpg')}}" style="background-size: cover)>
+    
+    <div class="login1"><h1>LOGIN</h1>
+    <div class="input1">
+        <input type="text" placeholder="username" name="" value=""></div>
+    <div class="input2">
+    <input type="password" name="" value="" placeholder="password">
+    <div class="input3">
+        
+        <input type="button" name="" value="sign in">
+
+    </div>
+</div></div>
+
+
+</body>
+</html>
